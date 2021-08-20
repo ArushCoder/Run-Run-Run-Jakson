bb58b26cfd2d798db29e189362dcf559e2c601db
ArushCoder <88230855+ArushCoder@users.noreply.github.com>
2021-08-20 14:12:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,13 +8,24 @@ function preload(){
 
 function setup(){
   createCanvas(400,400);
- 
-   Runner = createSprite(50,160,20,50)
-   Runner.loadAnimation("Runner_running")
-  
-  }
+  path = createSprite (200,200)
+  path.addImage (pathImage) 
+  path.velocityY = 4 
 
+   Runner = createSprite(195,360,20,50)
+   Runner.addAnimation("Runner_running", Runner_running)
+  Runner.scale = 0.06
+  
+  Leftedge = createSprite (0,0,100,800)
+  Leftedge.visible = false
+  Rightedge = createSprite (410,0,100,800)
+  Rightedge.visible = false
+}  
 function draw() {
   background(0);
-
+  if (path.y>400) {
+  path.y = height/2    
+  }
+ Runner.x = mouseX
+  drawSprites ()
 }
